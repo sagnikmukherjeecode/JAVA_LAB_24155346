30e0ce90d582468de7245eae133542623b530c18
Sagnik Mukherjee <24155346@kiit.ac.in>
2025-12-12 13:07:35 +0530

    day1PROG1

--- src/day1Prog1.java
+++ src/day1Prog1.java
@@ -1,7 +1,7 @@
 public class day1Prog1 {
     public static void main(String[] args) {
         System.out.println("NAME: SAGNIK MUKHERJEE");
-        System.out.println("ROLL: 24155346");
+        System.out.println("ROLL: 24155346 ");
         System.out.println("SECTION: CSE 33");
         System.out.println("BRANCH: COMPUTER SCIENCE ENGINEERING (AI & ML)");
 
