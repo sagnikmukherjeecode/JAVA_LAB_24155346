34b6384476a929e9fead070d301925a34b1ab4cb
Sagnik Mukherjee <24155346@kiit.ac.in>
2026-01-09 12:32:23 +0530

    day1PROG1

---
+++ src/day3Prog4.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class day3prog4_Account {
+    int acc_no;
+    double balance;
+
+    void input(Scanner sc) {
+        System.out.print("Enter account number: ");
+        acc_no = sc.nextInt();
+        System.out.print("Enter balance: ");
+        balance = sc.nextDouble();
+    }
+
+    void disp() {
+        System.out.println("Account No: " + acc_no + " Balance: " + balance);
+    }
+}
+
+class day3prog4_Person extends day3prog4_Account {
+    String name;
+    String aadhar_no;
+
+    void input(Scanner sc) {
+        super.input(sc);
+        System.out.print("Enter name: ");
+        name = sc.next();
+        System.out.print("Enter Aadhar No: ");
+        aadhar_no = sc.next();
+    }
+
+    void disp() {
+        super.disp();
+        System.out.println("Name: " + name + " Aadhar No: " + aadhar_no);
+    }
+}
+
+class day3prog4_Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        day3prog4_Person[] persons = new day3prog4_Person[3];
+        for (int i = 0; i < 3; i++) {
+            persons[i] = new day3prog4_Person();
+            System.out.println("Enter details of person " + (i + 1));
+            persons[i].input(sc);
+        }
+        System.out.println("Details of persons:");
+        for (int i = 0; i < 3; i++) {
+            persons[i].disp();
+        }
+    }
+}
---
+++ src/day3prog2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class day3prog2_plate {/**/
+    double length, width;
+    day3prog2_plate(double l, double w) {
+        length = l;
+        width = w;
+        System.out.println("Plate constructor: length=" + length + " width=" + width);
+    }
+}
+
+class day3prog2_box extends day3prog2_plate {
+    double height;
+    day3prog2_box(double l, double w, double h) {
+        super(l, w);
+        height = h;
+        System.out.println("Box constructor: length=" + length + " width=" + width + " height=" + height);
+    }
+}
+
+class day3prog2_woodbox extends day3prog2_box {
+    double thick;
+    day3prog2_woodbox(double l, double w, double h, double t) {
+        super(l, w, h);
+        thick = t;
+        System.out.println("WoodBox constructor: length=" + length + " width=" + width + " height=" + height + " thick=" + thick);
+    }
+}
+
+class day3prog2_Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length, width, height, thickness:");
+        double l = sc.nextDouble();
+        double w = sc.nextDouble();
+        double h = sc.nextDouble();
+        double t = sc.nextDouble();
+        day3prog2_woodbox wb = new day3prog2_woodbox(l, w, h, t);
+    }
+}
