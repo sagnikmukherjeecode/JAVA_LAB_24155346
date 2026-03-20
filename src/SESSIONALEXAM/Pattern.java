a2518e07ffcf28cccb86c2e89648da214552215d
Sagnik Mukherjee <24155346@kiit.ac.in>
2026-03-20 13:36:37 +0530

    1

---
+++ src/SESSIONALEXAM/Pattern.java
@@ -0,0 +1,17 @@
+package SESSIONALEXAM;
+import java.util.Scanner;
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j =n-1 ; j >=i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
